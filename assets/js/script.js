8534217638a28ec2ba422993297dbfe9df0b28bb
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2020-10-21 14:54:45 -0400

    added js file to contact and function for submit button. Also added assets folder for better organization

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// const submitBtn = document.getElementById("submitBtn");
+
+  $(function() {
+    $('#submitBtn').click(function() {
+        console.log("submitted!")
+    });
+});
+
+
+
+// // Name and Email validation Function.
+// function validation(){
+//     var name = document.getElementById("name").value;
+//     var email = document.getElementById("email").value;
+//     var emailReg = /^([w-.]+@([w-]+.)+[w-]{2,4})?$/;
+//     if( name ==='' || email ===''){
+//     alert("Please fill all fields...!!!!!!");
+//     return false;
+//     }else if(!(email).match(emailReg)){
+//     alert("Invalid Email...!!!!!!");
+//     return false;
+//     }else{
+//     return true;
+//     }
+//     }
