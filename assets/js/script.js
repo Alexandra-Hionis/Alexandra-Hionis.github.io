decb0b3e9305959f3afae9b2e87c34021466e95d
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2020-12-21 13:59:30 -0500

    updated contact section to look more attractive and added button. Made some other changes to files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,4 @@
-// Scroll bar
 
-// let progress = document.getElementById('progressbar');
-// let totalHeight = document.body.scrollHeight - window.innerHeight;
-// window.onscroll = function () {
-//   let progressHeight = (window.pageYOffset / totalHeight) * 100;
-//   progress.style.height = progressHeight + "%";
-// }
-
-// const submitBtn = document.getElementById("submitBtn");
-
-$(document).on('click',function(){
-  $('.collapse').collapse('hide');
-})
-
-// click submit button
-  $(function() {
-    $('#submitBtn').click(function() {
-        console.log("submitted!")
-    });
-});
-
-
-// Parallax background
-// document.addEventListener("mousemove" , parallax);
-// function parallax(e){
-//   this.querySelectorAll('.layer').forEach(layer => {
-//     const speed = layer.getAttribute('data-speed')
-//     const x = (window.innerwidth - e.pageX*speed)
-//     const x = (window.innerwidth - e.pageY*speed)
-
-//     layer.style.transform = 'translateX(${x}px) translateY(${y}px)'
-//   })
-// }
-
-// // Name and Email validation Function.
-// function validation(){
-//     var name = document.getElementById("name").value;
-//     var email = document.getElementById("email").value;
-//     var emailReg = /^([w-.]+@([w-]+.)+[w-]{2,4})?$/;
-//     if( name ==='' || email ===''){
-//     alert("Please fill all fields...!!!!!!");
-//     return false;
-//     }else if(!(email).match(emailReg)){
-//     alert("Invalid Email...!!!!!!");
-//     return false;
-//     }else{
-//     return true;
-//     }
-//     }
 
 // rotator
 // const text = document.getElementById('text');
@@ -101,6 +52,3 @@ sliders.forEach(slider => {
   appearOnScroll.observe(slider);
 });
 
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
