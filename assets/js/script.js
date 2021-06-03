4f4197b083610e120ece0407760f0ef32c9bf242
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-06-03 15:58:53 -0400

    Deleted contact form from contact page and restyled with new icons. Set up mailto link for my email and click to call/facetime depending on device. Edited some of the body text and css as well

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
-
-const faders = document.querySelectorAll('.fade-in');
+const faders = document.querySelectorAll(".fade-in");
 const header = document.querySelector("header");
 const sectionOne = document.querySelector(".home-intro");
-
 
 const sliders = document.querySelectorAll(".slide-in");
 
@@ -11,14 +9,16 @@ const appearOptions = {
   threshold: 0.5,
   // Add a root margin so once were 100 pixels from the bottom, it appears
   // Enhances the fade in expereince so it doesn't happen right when we get to the page
-  rootMargin: "0px 0px -100px 0px"
+  rootMargin: "0px 0px -100px 0px",
 };
 
-const appearOnScroll = new IntersectionObserver 
-(function(entries,appearOnScroll) {
-  entries.forEach(entry => {
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
     // if false then entry is intersection. Intersection observer, whether or not it's intersecting, is going to fire as soon as the page loads.
-    if(!entry.isIntersecting) {
+    if (!entry.isIntersecting) {
       return;
     } else {
       // stops looking once it's done its job
@@ -26,14 +26,12 @@ const appearOnScroll = new IntersectionObserver
       appearOnScroll.unobserve(entry.target);
     }
   });
-}, 
-appearOptions
-);
-faders.forEach(fader => {
+},
+appearOptions);
+faders.forEach((fader) => {
   appearOnScroll.observe(fader);
 });
 
-sliders.forEach(slider => {
+sliders.forEach((slider) => {
   appearOnScroll.observe(slider);
 });
-
